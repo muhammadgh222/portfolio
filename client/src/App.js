eecf8ea3f200d13894e28bc7959d205edd9e1878
devdavinci <dev.muhammad222000@gmail.com>
2023-03-28 16:59:47 +0200

    front end - navbar and hero section

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Navbar,
+  Hero,
+  Tech,
+  Works,
+  Experience,
+  Feedbacks,
+  StarsCanvas,
+} from "./components";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="relative z-0 bg-primary ">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
